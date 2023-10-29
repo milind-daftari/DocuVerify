88a17fd62ce1337e5df2975e6efb4a7352ec5199
Milind Daftari <milinddaftari@gmail.com>
2023-10-29 00:12:51 -0400

    Add amplify and attempt to link to S3 for Uploads

---
+++ docuverify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "docuverifyf3e26b9f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "docuVerify": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
