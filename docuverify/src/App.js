c7693a9a219193cbe53bb5c18322eb6a7ac38542
cratos <milinddaftari@gmail.com>
2023-12-08 16:16:35 -0500

    Update App.js
    Use HashRouter instead of BrowserRouter

--- docuverify/src/App.js
+++ docuverify/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import NavbarComponent from './components/NavbarComponents';
