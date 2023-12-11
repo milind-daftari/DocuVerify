330ad7c402712ac4f077a3ac792742a1c3e21ee3
Milind Daftari <milinddaftari@gmail.com>
2023-12-11 17:37:13 -0500

    Fix for Issue: Overwriting Documents in S3 Bucket #10

--- docuverify/src/components/Upload.js
+++ docuverify/src/components/Upload.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Button, Form, Alert, Card, Container, Row, Col } from 'react-bootstrap';
+import { Button, Form, Alert, OverlayTrigger, Tooltip, Row, Col } from 'react-bootstrap';
 import { Storage } from 'aws-amplify';
+import { v4 as uuidv4 } from 'uuid';
 
-function Upload() {
+function Upload({ user }) {
     const [error, setError] = useState('');
     const [success, setSuccess] = useState('');
     const [uploading, setUploading] = useState(false);
@@ -10,7 +11,27 @@ function Upload() {
     const [selectedFile, setSelectedFile] = useState(null);
 
     const handleFileChange = (e) => {
-        setSelectedFile(e.target.files[0]);
+        const file = e.target.files[0];
+        if (file) {
+            const fileName = file.name;
+
+            if (!/^[a-zA-Z0-9.-_]+$/.test(fileName)) {
+                setError('File name should only have alphanumeric characters, hyphens, underscores, and dots.');
+                e.target.value = '';
+                return;
+            }
+
+            const fileExtension = fileName.split('.').pop().toLowerCase();
+            if (fileExtension !== 'pdf') {
+                setError('Invalid file type. Please upload a PDF.');
+                e.target.value = '';
+                return;
+            }
+
+            setError('');
+            setSuccess('');
+            setSelectedFile(file);
+        }
     };
 
     const handleDescriptionChange = (e) => {
@@ -23,16 +44,22 @@ function Upload() {
             setSuccess('');
             return;
         }
-        setError('');
+
+        const uniqueFileName = `${uuidv4()}_${selectedFile.name}`;
         setUploading(true);
         try {
-            const result = await Storage.put(selectedFile.name, selectedFile, {
+            await Storage.put(uniqueFileName, selectedFile, {
                 contentType: selectedFile.type,
-                metadata: { description: documentDescription }
+                metadata: {
+                    description: documentDescription,
+                    originalFileName: selectedFile.name,
+                    uploadTimestamp: new Date().toISOString(),
+                    username: user.username,
+                    metamaskAddress: user.metaMaskAddress,
+                    fileSize: selectedFile.size.toString()
+                }
             });
-            console.log(result); // For debugging purposes
             setSuccess('Upload successful!');
-            // Clear the form data
             setSelectedFile(null);
             setDocumentDescription('');
             setError('');
@@ -51,42 +78,44 @@ function Upload() {
     };
 
     return (
-        <Container className="mt-5">
-            <Row className="justify-content-center">
-                <Col lg={8} md={10}>
-                    <Card className="shadow-sm p-4">
-                        <Card.Body>
-                            <h5 className="text-center mb-4">Upload Document</h5>
-                            {success && <Alert variant="success">{success}</Alert>}
-                            {error && <Alert variant="danger">{error}</Alert>}
-                            <Form onSubmit={handleSubmit}>
-                                <Form.Group className="mb-3" controlId="formFile">
-                                    <Form.Label>Upload your document</Form.Label>
-                                    <Form.Control type="file" onChange={handleFileChange} disabled={uploading} />
-                                </Form.Group>
-                                <Form.Group className="mb-3" controlId="formDescription">
-                                    <Form.Label>Document Description (max 128 characters)</Form.Label>
-                                    <Form.Control 
-                                        as="textarea" 
-                                        rows={3}
-                                        placeholder="Enter a brief description of the document" 
-                                        value={documentDescription}
-                                        onChange={handleDescriptionChange}
-                                        maxLength={128}
-                                        disabled={uploading}
-                                    />
-                                </Form.Group>
-                                <div className="d-grid">
-                                    <Button variant="primary" size="lg" type="submit" disabled={uploading}>
-                                        {uploading ? 'Uploading...' : 'Upload'}
-                                    </Button>
-                                </div>
-                            </Form>
-                        </Card.Body>
-                    </Card>
+        <div className="container" style={{ marginTop: '4rem' }}>
+            <Row className="justify-content-center align-items-center pt-5">
+                <Col md={6}>
+                    <h2 className="text-center mb-4">Upload Document</h2>
+                    {success && <Alert variant="success">{success}</Alert>}
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group controlId="formFile" className="mb-3">
+                            <Form.Label>Upload your document</Form.Label>
+                            <OverlayTrigger
+                                placement="right"
+                                overlay={
+                                    <Tooltip>
+                                        File size limit is 5MB. File name should contain alphanumeric characters, hyphens, and underscores only. Only PDF files are allowed.
+                                    </Tooltip>
+                                }>
+                                <Form.Control type="file" onChange={handleFileChange} disabled={uploading} />
+                            </OverlayTrigger>
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Document Description (max 128 characters)</Form.Label>
+                            <Form.Control 
+                                type="text" 
+                                placeholder="Enter a brief description of the document" 
+                                value={documentDescription}
+                                onChange={handleDescriptionChange}
+                                maxLength={128}
+                            />
+                        </Form.Group>
+                        <div className="d-flex justify-content-center">
+                            <Button variant="primary" type="submit" disabled={uploading}>
+                                {uploading ? 'Uploading...' : 'Upload'}
+                            </Button>
+                        </div>
+                    </Form>
                 </Col>
             </Row>
-        </Container>
+        </div>
     );
 }
 
--- docuverify/src/components/Verify.js
+++ docuverify/src/components/Verify.js
@@ -1,21 +1,32 @@
 import React, { useState } from 'react';
-import { Button, Form, Alert, Card, Row, Col } from 'react-bootstrap';
+import { Button, Form, Alert, OverlayTrigger, Tooltip, Card, Row, Col } from 'react-bootstrap';
+import { Storage } from 'aws-amplify';
+import { v4 as uuidv4 } from 'uuid';
 
-function Verify() {
+function Verify({ user }) {
     const [error, setError] = useState('');
     const [selectedFile, setSelectedFile] = useState(null);
+    const [metaMaskAddressToValidate, setMetaMaskAddressToValidate] = useState('');
+    const [success, setSuccess] = useState('');
+
+    const isMetaMaskAddressValid = (address) => {
+        const regex = /^0x[a-fA-F0-9]{40}$/;
+        return regex.test(address);
+    };
 
     const handleFileChange = (e) => {
         const file = e.target.files[0];
         if (file) {
             const fileName = file.name;
 
-            if (/[^a-zA-Z0-9.-]/.test(fileName.split('.').shift()) || (fileName.match(/\./g) || []).length > 1) {
-                setError('File name should not have special characters.');
+            // Check for valid file name
+            if (!/^[a-zA-Z0-9.-_]+$/.test(fileName)) {
+                setError('File name should only have alphanumeric characters, hyphens, underscores, and dots.');
                 e.target.value = '';
                 return;
             }
 
+            // Check for valid file type (PDF)
             const fileExtension = fileName.split('.').pop().toLowerCase();
             if (fileExtension !== 'pdf') {
                 setError('Invalid file type. Please upload a PDF.');
@@ -23,14 +34,57 @@ function Verify() {
                 return;
             }
 
+            // Check for file size (5MB limit)
+            if (file.size > 5242880) {
+                setError('File size should not exceed 5MB.');
+                e.target.value = '';
+                return;
+            }
+
             setError('');
+            setSuccess('');
             setSelectedFile(file);
         }
     };
 
-    const handleSubmit = (e) => {
+    const handleMetaMaskAddressChange = (e) => {
+        setMetaMaskAddressToValidate(e.target.value);
+    };
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        // Add your file verification logic here
+        if (!selectedFile) {
+            setSuccess('');
+            setError('Please upload a file for verification.');
+            return;
+        }
+        if (!isMetaMaskAddressValid(metaMaskAddressToValidate)) {
+            setSuccess('');
+            setError('Invalid MetaMask address. Please check and try again.');
+            return;
+        }
+
+        const uniqueFileName = `tmp/${uuidv4()}_${selectedFile.name}`;
+        try {
+            await Storage.put(uniqueFileName, selectedFile, {
+                contentType: selectedFile.type,
+                metadata: {
+                    originalFileName: selectedFile.name,
+                    uploadTimestamp: new Date().toISOString(),
+                    username: user.username,
+                    userAddress: user.metaMaskAddress,
+                    toValidateFor: metaMaskAddressToValidate
+                }
+            });
+            setError('');
+            setSuccess('File uploaded for verification.');
+            setSelectedFile(null);
+            setMetaMaskAddressToValidate('');
+        } catch (err) {
+            console.error('Error uploading the file for verification: ', err);
+            setError('Error uploading the file for verification.');
+            setSuccess('');
+        }
     };
 
     return (
@@ -40,14 +94,32 @@ function Verify() {
                     <Card>
                         <Card.Body>
                             <Card.Title>Verify Document</Card.Title>
+                            {success && <Alert variant="success">{success}</Alert>}
                             {error && <Alert variant="danger">{error}</Alert>}
                             <Form onSubmit={handleSubmit}>
                                 <Form.Group controlId="formFileVerify" className="mb-3">
                                     <Form.Label>Upload your document for verification</Form.Label>
-                                    <Form.Control type="file" onChange={handleFileChange} />
+                                    <OverlayTrigger
+                                        placement="right"
+                                        overlay={
+                                            <Tooltip>
+                                                File size limit is 5MB. File name should contain alphanumeric characters, hyphens, and underscores only. Only PDF files are allowed.
+                                            </Tooltip>
+                                        }>
+                                        <Form.Control type="file" onChange={handleFileChange} />
+                                    </OverlayTrigger>
+                                </Form.Group>
+                                <Form.Group controlId="metaMaskAddress" className="mb-3">
+                                    <Form.Label>MetaMask Address to Validate Against</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Enter MetaMask address"
+                                        value={metaMaskAddressToValidate}
+                                        onChange={handleMetaMaskAddressChange}
+                                    />
                                 </Form.Group>
                                 <Button variant="primary" type="submit">
-                                    Verify
+                                    Submit for Verification
                                 </Button>
                             </Form>
                         </Card.Body>
