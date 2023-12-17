4cbada6f112a4fc2d252abcadbb746160febdd64
Rana-KV <pranavakumar196@gmail.com>
2023-12-16 23:03:55 -0500

    Updated smart contract and created intreact.js

--- docuverifySmartContract/contract/docuverifySmartContract.sol
+++ docuverifySmartContract/contract/docuverifySmartContract.sol
@@ -4,14 +4,15 @@ pragma solidity ^0.8.23;
 contract docuverifySmartContract {
     // Structure to hold document details
     struct Document {
-        string name;
-        string details;
-        address uploadedBy;
         uint256 timestamp;
+        bytes32 hash;
     }
 
-    // Mapping of document hash to Document
-    mapping (string => Document) private documents;
+    // Mapping of user address to an array of Documents
+    mapping (address => Document[]) private userDocuments;
+
+    // Mapping to keep track of registered addresses
+    mapping (address => bool) private isRegistered;
 
     // Admin address is set to the address that deploys the contract
     address public admin;
@@ -21,7 +22,10 @@ contract docuverifySmartContract {
     uint256 public constant verifyFee = 1e13;
 
     // Event to emit when a document is registered
-    event DocumentRegistered(string indexed hash, address indexed uploadedBy, uint256 timestamp);
+    event DocumentRegistered(address indexed user, bytes32 indexed hash, uint256 timestamp);
+
+    // Event to emit when a document is verified
+    event DocumentVerified(uint256 timestamp, bytes32 hash);
 
     // Constructor to set the admin
     constructor() {
@@ -33,31 +37,38 @@ contract docuverifySmartContract {
         require(msg.sender == admin, "Caller is not the admin");
         _;
     }
-
+    
     // Function to register a document
-    function registerDocument(string calldata hash, string calldata name, string calldata details) external payable {
-        require(msg.value == uploadFee, "Incorrect upload fee");
-        require(bytes(hash).length > 0, "Document hash cannot be empty");
-        require(bytes(name).length > 0, "Document name cannot be empty");
-        require(bytes(details).length > 0, "Document details cannot be empty");
-        require(documents[hash].timestamp == 0, "Document already registered");
+    function registerDocument(bytes32 hash, uint256 timestamp) external payable{
+        require(msg.value >= uploadFee, "Upload fee is not sufficient");
 
-        documents[hash] = Document(name, details, msg.sender, block.timestamp);
-        emit DocumentRegistered(hash, msg.sender, block.timestamp);
+        // Check if the document already exists for the user
+        for (uint i = 0; i < userDocuments[msg.sender].length; i++) {
+            require(userDocuments[msg.sender][i].hash != hash, "Document already registered");
+        }
+
+        userDocuments[msg.sender].push(Document(timestamp, hash));
+        emit DocumentRegistered(msg.sender, hash, timestamp);
     }
 
     // Function to verify a document by its hash
-    function verifyDocumentByHash(string calldata hash) external payable returns (string memory, string memory, address, uint256) {
-        require(msg.value == verifyFee, "Incorrect verification fee");
-        require(bytes(hash).length > 0, "Document hash cannot be empty");
-        require(documents[hash].timestamp != 0, "Document not found");
+    function verifyDocument(bytes32 hash, address user) external view returns (bool, uint256, bytes32) {
 
-        Document memory doc = documents[hash];
-        return (doc.name, doc.details, doc.uploadedBy, doc.timestamp);
+        for (uint i = 0; i < userDocuments[user].length; i++) {
+            if(userDocuments[user][i].hash == hash) {
+                return (true, userDocuments[user][i].timestamp, hash);
+            }
+        }
+        return (false, 0, 0);
     }
 
     // Function to withdraw accumulated fees
     function withdrawFunds() external onlyAdmin {
         payable(admin).transfer(address(this).balance);
+    }
+
+    // Function to get the current balance of the contract
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 }
